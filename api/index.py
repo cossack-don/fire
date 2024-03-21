ff8dc61831a9a3ce8aba2305c139888cad6caec2
cossack-done <ser90593696.zhuravlev@yandex.ru>
2024-03-21 22:51:22 +0300

    test api

--- api/index.py
+++ api/index.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/api")
+@app.get("/api/v1/test")
 def hello_world():
-    return {"message": "Hello World"}
+    return {"message": "Hello World222"}
+
+@app.get("/api/v1/test2")
+def hello_world2():
+    return {"message": "Hello dadadad"}
